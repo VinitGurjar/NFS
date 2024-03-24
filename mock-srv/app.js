2ad34714e56533bdf30e0bbaf171b4c5010174d0
VinitGurjar <vinitfagna@gmail.com>
2024-03-24 23:51:33 +0530

    feat(CORS): setup cors access, register the CORS plugin and load the plugins and routes using AutoLoad

--- mock-srv/app.js
+++ mock-srv/app.js
@@ -1,30 +1,33 @@
-import path from 'path'
-import AutoLoad from '@fastify/autoload'
-import { fileURLToPath } from 'url'
+/*
+ * setup CORS access
+ * Create our two GET routes
+ */
+import path from "node:path";
+import AutoLoad from "@fastify/autoload";
+import { fileURLToPath } from "node:url";
+import cors from "@fastify/cors";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Pass --options via CLI arguments in command to enable these options.
-export const options = {}
+export const options = {};
 
 export default async function (fastify, opts) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
+  fastify.register(cors);
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
-  // through your application
+  // through this application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
 }
